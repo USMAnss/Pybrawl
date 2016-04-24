d5ba0d272e1bb145416b7ee59b4c48fdf7a90d81
USMAnss <nick1994@abv.bg>
2016-04-24 09:48:18 -0400

    Add new sprites

--- main.py
+++ main.py
@@ -18,54 +18,61 @@ WIDTH=800
 HEIGHT=600
 FPS=60
 
-NARUTO_IMAGES = {"stance": ["Naruto/naruto_stance1.gif",
-                            "Naruto/naruto_stance2.gif",
-                            "Naruto/naruto_stance3.gif",
-                            "Naruto/naruto_stance4.gif"],
-                 "runright": ["Naruto/naruto_runright1.gif",
-                              "Naruto/naruto_runright2.gif",
-                              "Naruto/naruto_runright3.gif",
-                              "Naruto/naruto_runright4.gif",
-                              "Naruto/naruto_runright5.gif",
-                              "Naruto/naruto_runright6.gif"],
-                 "runleft": ["Naruto/naruto_runleft1.gif",
-                              "Naruto/naruto_runleft2.gif",
-                              "Naruto/naruto_runleft3.gif",
-                              "Naruto/naruto_runleft4.gif",
-                              "Naruto/naruto_runleft5.gif",
-                              "Naruto/naruto_runleft6.gif"],
-                 "jump": ["Naruto/naruto_jump1.gif",
-                          "Naruto/naruto_jump2.gif",
-                          "Naruto/naruto_jump3.gif",
-                          "Naruto/naruto_jump4.gif"],
-                 "attack": ["Naruto/naruto_normalattack1.gif",
-                            "Naruto/naruto_normalattack2.gif",
-                            "Naruto/naruto_normalattack3.gif"],
+NARUTO_IMAGES = {"stance": ["Naruto/new_sprites/stance0.png",
+                            "Naruto/new_sprites/stance1.png",
+                            "Naruto/new_sprites/stance2.png",
+                            "Naruto/new_sprites/stance3.png"],
+                 "runright": ["Naruto/new_sprites/move_right0.png",
+                              "Naruto/new_sprites/move_right1.png",
+                              "Naruto/new_sprites/move_right2.png",
+                              "Naruto/new_sprites/move_right3.png",
+                              "Naruto/new_sprites/move_right4.png",
+                              "Naruto/new_sprites/move_right5.png"],
+                 "runleft": ["Naruto/new_sprites/move_left0.png",
+                             "Naruto/new_sprites/move_left1.png",
+                             "Naruto/new_sprites/move_left2.png",
+                             "Naruto/new_sprites/move_left3.png",
+                             "Naruto/new_sprites/move_left4.png",
+                             "Naruto/new_sprites/move_left5.png"],
+                 "jump": ["Naruto/new_sprites/jump0.png",
+                          "Naruto/new_sprites/jump1.png",
+                          "Naruto/new_sprites/jump2.png",
+                          "Naruto/new_sprites/jump4.png",
+                          "Naruto/new_sprites/jump5.png",
+                          "Naruto/new_sprites/jump6.png"],
+                 "attack": ["Naruto/new_sprites/normal_attack0.png",
+                            "Naruto/new_sprites/normal_attack1.png",
+                            "Naruto/new_sprites/normal_attack2.png"],
                  "icon": "Hud/naruto_icon.png"}
 
-SASUKE_IMAGES = {"stance": ["Sasuke/sasuke_stance1.gif",
-                            "Sasuke/sasuke_stance2.gif",
-                            "Sasuke/sasuke_stance3.gif",
-                            "Sasuke/sasuke_stance4.gif"],
-                 "runright": ["Sasuke/sasuke_runright1.gif",
-                              "Sasuke/sasuke_runright2.gif",
-                              "Sasuke/sasuke_runright3.gif",
-                              "Sasuke/sasuke_runright4.gif",
-                              "Sasuke/sasuke_runright5.gif",
-                              "Sasuke/sasuke_runright6.gif"],
-                 "runleft": ["Sasuke/sasuke_runleft1.gif",
-                              "Sasuke/sasuke_runleft2.gif",
-                              "Sasuke/sasuke_runleft3.gif",
-                              "Sasuke/sasuke_runleft4.gif",
-                              "Sasuke/sasuke_runleft5.gif",
-                              "Sasuke/sasuke_runleft6.gif"],
-                 "jump": ["Sasuke/sasuke_jump1.gif",
-                          "Sasuke/sasuke_jump2.gif",
-                          "Sasuke/sasuke_jump3.gif",
-                          "Sasuke/sasuke_jump4.gif"],
-                 "attack": ["Sasuke/sasuke_normalattack1.gif",
-                            "Sasuke/sasuke_normalattack2.gif",
-                            "Sasuke/sasuke_normalattack3.gif"],
+SASUKE_IMAGES = {"stance": ["Sasuke/new_sprites/stance0.gif",
+                            "Sasuke/new_sprites/stance1.gif",
+                            "Sasuke/new_sprites/stance2.gif",
+                            "Sasuke/new_sprites/stance3.gif",
+                            "Sasuke/new_sprites/stance4.gif",
+                            "Sasuke/new_sprites/stance5.gif"],
+                 "runright": ["Sasuke/new_sprites/move_right0.gif",
+                              "Sasuke/new_sprites/move_right1.gif",
+                              "Sasuke/new_sprites/move_right2.gif",
+                              "Sasuke/new_sprites/move_right3.gif",
+                              "Sasuke/new_sprites/move_right4.gif",
+                              "Sasuke/new_sprites/move_right5.gif"],
+                 "runleft": ["Sasuke/new_sprites/move_left0.gif",
+                             "Sasuke/new_sprites/move_left1.gif",
+                             "Sasuke/new_sprites/move_left2.gif",
+                             "Sasuke/new_sprites/move_left3.gif",
+                             "Sasuke/new_sprites/move_left4.gif",
+                             "Sasuke/new_sprites/move_left5.gif"],
+                 "jump": ["Sasuke/new_sprites/jump0.gif",
+                          "Sasuke/new_sprites/jump1.gif",
+                          "Sasuke/new_sprites/jump2.gif",
+                          "Sasuke/new_sprites/jump4.gif",
+                          "Sasuke/new_sprites/jump5.gif",
+                          "Sasuke/new_sprites/jump6.gif"],
+                 "attack": ["Sasuke/new_sprites/normal_attack0.png",
+                            "Sasuke/new_sprites/normal_attack1.png",
+                            "Sasuke/new_sprites/normal_attack2.png",
+                            "Sasuke/new_sprites/normal_attack3.png"],
                  "icon": "Hud/sasuke_icon.png"}
 
 HUD_IMAGES = {"frame": "Hud/frame.png",
@@ -138,7 +145,7 @@ def playGame():
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
-                sys.exit()     
+                sys.exit()
         player1.update(pygame.key.get_pressed())
         player2.update(pygame.key.get_pressed())
 
@@ -146,7 +153,7 @@ def playGame():
 
         keep_inside(player1)
         keep_inside(player2)
-        
+
         if player1.rect.centerx < player2.rect.centerx:
             player1.direction="right"
             player2.direction="left"
@@ -158,7 +165,7 @@ def playGame():
                            player1.mana/player1.max_mana)
         player2_hud.update(player2.health/player2.max_health,
                            player2.mana/player2.max_mana)
-        
+
         player1.draw(displaysurf)
         player2.draw(displaysurf)
         player1_hud.draw(displaysurf)
